3b4918d799c643d16a154b40f0bd899e8301898d
hoangton0934 <tonlongthanhhoang@gmail.com>
2024-06-11 15:58:14 +0700

    set up server for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { v2 as cloudinary } from "cloudinary";
-
+import path from "path";
 
 import notificationRoutes from "./routes/notification.route.js";
 import postRoutes from "./routes/post.route.js";
@@ -21,6 +21,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000 ; 
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" })); // to parse req.body
 // limit shouldn't be too high to prevent DOS
@@ -36,6 +37,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);   
 app.use("/api/posts", postRoutes);   
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
 
 app.listen(PORT, () =>{
 console.log(`Server is running on port ${PORT}`);
